e5471ff7952e17ac7870b46e3cd18d5e0f6d3106
ianian-c-y <161934550+ianian-c-y@users.noreply.github.com>
2026-07-15 13:09:18 +1000

    /

--- Script/Instagram-Download-Button.user.js
+++ Script/Instagram-Download-Button.user.js
@@ -738,10 +738,10 @@
                 .pop();
         }
         fetch(url, {
-            headers: new Headers({
-                                     'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 12_3_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 Instagram 105.0.0.11.118 (iPhone11,8; iOS 12_3_1; en_US; en-US; scale=2.00; 828x1792; 165586599)', //window.navigator.userAgent,
-                                     Origin: location.origin,
-                                 }),
+            // headers: new Headers({
+            //                          'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 12_3_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 Instagram 105.0.0.11.118 (iPhone11,8; iOS 12_3_1; en_US; en-US; scale=2.00; 828x1792; 165586599)', //window.navigator.userAgent,
+            //                          Origin: location.origin,
+            //                      }),
             mode: 'cors',
         })
             .then(response => response.blob())
